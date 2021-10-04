551da9a398544e4b98a48577267e43eb360777b9
Filipi Silva <filipinascimento@gmail.com>
2021-10-03 20:33:28 -0400

    Bumped to version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="RModularity",
-    version="0.1.0",
+    version="0.1.1",
     author="Filipi N. Silva",
     author_email="filipi@iu.edu",
     description="Python library to calculate Robustness modularity of networks",
