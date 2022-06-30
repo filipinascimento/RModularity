82898a007db802c92f356b35a21c5eeed94bbffe
Filipi Nascimento Silva <filipinascimento@gmail.com>
2022-06-30 18:09:46 +0000

    Fixed dependencies in pip install - bumped to 0.2.1 Note: still needs graph-tool installed separately

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf8") as fh:
+    requirements = fh.readlines()
 
 setuptools.setup(
     name="RModularity",
-    version="0.2.0",
+    version="0.2.1",
     author="Filipi N. Silva",
     author_email="filipi@iu.edu",
     description="Python library to calculate Robustness modularity of networks",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=[req for req in requirements if req[:2] != "# "],
     url="https://github.com/filipinascimento/RModularity",
     packages=setuptools.find_packages(),
     classifiers=[
